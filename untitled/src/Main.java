ebecfc3b78bdaf6f99ab85a78415471682e729d8
DienVuong <vuongdien261@gmail.com>
2022-10-05 23:23:55 +0700

    thực hành giải pt bậc nhất

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("a:");
+        a = sc.nextDouble();
+        System.out.println("b:");
+        b = sc.nextDouble();
+        System.out.println("c:");
+        c = sc.nextDouble();
+        if(a != 0){
+            double answer = (c-b) / a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        }  else {
+            if(b == c){
+                System.out.printf("The solution is all x!");
+            }else {
+                System.out.printf("No solution!");
+            }
+        }
     }
 }
