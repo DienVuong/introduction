d066ed0cf8f6a073ef68a77d834edacb21e786d3
DienVuong <vuongdien261@gmail.com>
2022-10-05 21:25:49 +0700

    Thực hành hiển thị thời gian của hệ thông

---
+++ untitled/src/System_time.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class System_time {
+    public static void main(String[] args) {
+//        Lấy ra time hiênj tại
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
